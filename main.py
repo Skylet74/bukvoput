e883f616020aaf465e0819be22556d2e633b9065
Skylet74 <115532457+Skylet74@users.noreply.github.com>
2022-10-12 11:48:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import sys
 import telebot;
+Vowels = "аоуэыяёюеи"
+Consonants = "бвгджзйклмнпрстфхцчшщ"
+
+def CheckDoubledLettersType(word1,word2):
+    if (word1[1] in Consonants and word1[2] in Consonants) or
+       (word2[1] in Consonants and word2[2] in Consonants) or
+       (word1[1] in Vowels and word1[2] in Vowels) or
+       (word2[1] in Vowels and word2[2] in Vowels):
+        result = true
+    else:
+        result = false
+    return result
+    
 
 def changeFirstLetters(words):
     if len(words) < 2:
@@ -10,17 +23,24 @@ def changeFirstLetters(words):
     letter2 = word2[0]
     word1 = letter2 + word1[1:]
     word2 = letter1 + word2[1:]
-return word1 + " " + word2
-
+    return word1 + " " + word2
+    
+def Bukvoput(words):
+    words = changeFirstLetters(words)
+    if CheckDoubledLettersType(words[0],words[1]):
+        return "Получается не смэшно"
+    else:
+        return words
+    
 bot = telebot.TeleBot('5519104894:AAGw3YLcJfC3uzg4UMeCnXwiWDmRUIFLkgY');
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     words = message.text.split()
     try:
-        bot.send_message(message.chat.id, changeFirstLetters(words))
+        bot.send_message(message.chat.id, Bukvoput(words))
     except:
-        bot.send_message(message.from_user.id, changeFirstLetters(words))
+        bot.send_message(message.from_user.id, Bukvoput(words))
 
 
 
